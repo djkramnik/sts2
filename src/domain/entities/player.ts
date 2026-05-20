50b563e632707f46ee88507887c7eed00e27f9e8
David Gurr <davidk.gurr@gmail.com>
2026-05-20 15:15:22 -0400

    fumbling along

--- src/domain/entities/player.ts
+++ src/domain/entities/player.ts
@@ -1,16 +1,31 @@
+import { Card } from "./card"
+
 type PlayerState = {
   maxHp: number
   hp: number
   mana: number
+  deck: Card[]
 }
 
 export class Player {
   maxHp: number
   hp: number
   mana: number
-  constructor({ maxHp, hp, mana }: PlayerState) {
+  deck: Card[]
+  base: PlayerState
+  constructor({ maxHp, hp, mana, deck }: PlayerState) {
     this.maxHp = maxHp
     this.hp = hp
     this.mana = mana
+    this.deck = deck
+    this.base = {maxHp, hp, mana, deck}
   }
+
+  restoreBase() {
+    this.maxHp = this.base.maxHp
+    this.hp = this.base.hp
+    this.mana = this.base.mana
+    this.deck = this.base.deck
+  }
+
 }
