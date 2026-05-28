f0f74f24be7dd959d514dfa678c07e1a8d46a737
David Gurr <davidk.gurr@gmail.com>
2026-05-27 20:41:49 -0400

    stub ii

---
+++ packages/ui/pages/_app.tsx
@@ -0,0 +1,18 @@
+import '../styles/_global.css'
+import "@/styles/index.css"
+import type { AppProps } from "next/app"
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import { theme } from '../components/theme'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const activeTheme = theme
+
+  return (
+    <>
+      <ThemeProvider theme={activeTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
